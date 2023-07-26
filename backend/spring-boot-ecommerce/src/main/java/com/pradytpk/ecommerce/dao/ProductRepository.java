c1220a0c564db226c52fafaa277d81f448853f0c
Pradeep Kumar <61448614+pradytpk@users.noreply.github.com>
2023-07-26 21:19:50 +0900

    Search for Products by Category

--- backend/spring-boot-ecommerce/src/main/java/com/pradytpk/ecommerce/dao/ProductRepository.java
+++ backend/spring-boot-ecommerce/src/main/java/com/pradytpk/ecommerce/dao/ProductRepository.java
@@ -1,6 +1,9 @@
 package com.pradytpk.ecommerce.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.pradytpk.ecommerce.entity.Product;
@@ -8,4 +11,5 @@ import com.pradytpk.ecommerce.entity.Product;
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long>{
 
+	Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
