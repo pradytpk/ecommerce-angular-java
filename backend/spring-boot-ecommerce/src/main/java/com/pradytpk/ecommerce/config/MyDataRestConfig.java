bd3d2de2bd8c4cd8851944ec8622cd364df85258
Pradeep Kumar <61448614+pradytpk@users.noreply.github.com>
2023-07-29 08:40:15 +0900

    Search for Products by Category

--- backend/spring-boot-ecommerce/src/main/java/com/pradytpk/ecommerce/config/MyDataRestConfig.java
+++ backend/spring-boot-ecommerce/src/main/java/com/pradytpk/ecommerce/config/MyDataRestConfig.java
@@ -1,5 +1,9 @@
 package com.pradytpk.ecommerce.config;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -8,9 +12,17 @@ import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import com.pradytpk.ecommerce.entity.Product;
 import com.pradytpk.ecommerce.entity.ProductCategory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
 
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer{
+	
+	private EntityManager entityManager;
+	
+	public MyDataRestConfig(EntityManager theEntityManager) {
+		entityManager = theEntityManager;
+	}
 
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -34,6 +46,21 @@ public class MyDataRestConfig implements RepositoryRestConfigurer{
 	
 		config.setDefaultMediaType(MediaType.APPLICATION_JSON);
         config.useHalAsDefaultJsonMediaType(false);
+        
+        // call an internal helper method
+        exposeIds(config);	
+		
+	}
+	
+	private void exposeIds(RepositoryRestConfiguration config) {
+		Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+		List<Class> entityClasses = new ArrayList<>();
+		
+		for(EntityType tempEntityType:entities) {
+			entityClasses.add(tempEntityType.getJavaType());
+			Class[] domainTypes= entityClasses.toArray(new Class[0]);
+			config.exposeIdsFor(domainTypes);
+		}
 		
 	}
 
